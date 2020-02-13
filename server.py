68e699ad7f6791feeaf13d51f1081d6ebe5c03d3
Nathaniel Ho <nxho93@gmail.com>
2020-02-13 15:12:06 -0800

    523rd try's the charm

--- server.py
+++ server.py
@@ -375,8 +375,7 @@ def get_sio_with_handlers(username, player_uuid, room_id, cache):
 
     server_url = 'http://localhost:5000'
     if os.getenv('MAHJONG_ENV', 'dev') == 'heroku':
-        # FIXME: I don't like declaring this like this at all. investigate not hardcoding web address
-        server_url = 'https://mahjong-server-dev.herokuapp.com'
+        server_url = 'http://localhost'
 
     sio.connect(server_url)
 
