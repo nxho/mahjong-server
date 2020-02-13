55a4fc0770ce9b88b683f57e39bec553074d0ace
Nathaniel Ho <nxho93@gmail.com>
2020-02-13 15:06:08 -0800

    Change server url for ai client again

--- server.py
+++ server.py
@@ -375,7 +375,8 @@ def get_sio_with_handlers(username, player_uuid, room_id, cache):
 
     server_url = 'http://localhost:5000'
     if os.getenv('MAHJONG_ENV', 'dev') == 'heroku':
-        server_url = 'http://0.0.0.0:80'
+        # FIXME: I don't like declaring this like this at all. investigate not hardcoding web address
+        server_url = 'http://mahjong-server-dev.herokuapp.com'
 
     sio.connect(server_url)
 
